287181c7c2fe0da4d4ecdd49d87e4da55a9080d1
João Breches <57300761+Bseancy@users.noreply.github.com>
2021-09-24 17:27:26 -0300

    Funções modo noturno claro (não pronto)
    A base das funções para mudar do modo claro para o noturno, não está pronto, é apenas para ter uma base de início, devem ser feitas as alterações baseadas na forma que queremos que fique no site.

---
+++ PROA - HISTORIA/exercicios/js/not-claro.js
@@ -0,0 +1,14 @@
+function mudarEstilosNot()  {
+    text.style.fontFamily = "Arial";
+    text.style.color = "white";
+    text.style.backgroundColor = "black";
+
+}
+
+function mudarEstilosClaro()  {
+    text.style.fontFamily = "Arial";
+    text.style.color = "black";
+    text.style.backgroundColor = "grey";
+
+}
+
